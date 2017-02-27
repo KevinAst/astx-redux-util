785b1860fd53db2843432966fd61bbf8fec7ea4c
Kevin Bridges <kevin@wiiBridges.com>
2017-02-27 10:12:40 -0600

    added conditionalReducer unit tests

---
+++ src/reducer/spec/conditionalReducer.spec.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import expect                from 'expect';
+import {conditionalReducer}  from '../../index'; // module under test (NOTE: we purposely vary import techniques)
+
+const initialState  = 'initialState';
+const thenState     = 'thenState';
+const elseState     = 'elseState';
+
+const thenAction    = 'thenAction';
+
+const elseCondition = 'elseContidion';
+
+const thenReducer   = (state, action) => thenState;
+const elseReducer   = (state, action) => elseState;
+
+function performTest(reducer, actionType, expectedState) {
+  it(`process: '${actionType}'`, () => {
+    expect(reducer(initialState, {type: actionType})).toBe(expectedState);
+  });
+}
+
+describe('conditionalReducer() tests', () => {
+
+
+  describe('conditionalReducer with if only', () => {
+
+    const reducerUnderTest = conditionalReducer(
+      (state, action, originalReducerState) => action.type === thenAction,
+      thenReducer
+    );
+
+    performTest(reducerUnderTest, thenAction,    thenState);
+    performTest(reducerUnderTest, elseCondition, initialState);
+
+  });
+
+
+  describe('conditionalReducer with if/then', () => {
+
+    const reducerUnderTest = conditionalReducer(
+      (state, action, originalReducerState) => action.type === thenAction,
+      thenReducer,
+      elseReducer
+    );
+
+    performTest(reducerUnderTest, thenAction,    thenState);
+    performTest(reducerUnderTest, elseCondition, elseState);
+
+  });
+
+  // TODO: test edge case: validating parameters
+});
